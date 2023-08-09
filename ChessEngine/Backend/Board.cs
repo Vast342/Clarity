357e34938dd1636c2f80a0208632674747d93191
Vast342 <jcpasfield@gmail.com>
2023-08-09 00:06:06 -0400

    First Half Of Pseudo-legal Move Generation

--- ChessEngine/Backend/Board.cs
+++ ChessEngine/Backend/Board.cs
@@ -9,14 +9,14 @@ namespace Chess {
         public const int King = 6;
 
         public const int White = 8;
-        public const int Black = 16;
-        
+        public const int Black = 0;
     }
-    public static class Board {
-        public static int[] squares;
-        public static Board() {
-            squares = new int[64];
-
+    public class Board {
+        public static int[] squares = new int[64];
+        public static int colorToMove = 1;
+        public Board(string fen) {
+            LoadFenToPosition(fen);
+            MoveData();
         }
         /// <summary>
         /// Loads the information contained within a string of Forsyth–Edwards Notation into the current board instance's values.*
@@ -26,10 +26,10 @@ namespace Chess {
             var pieceCharDictionary = new Dictionary<char, int>() {
                 ['p'] = Piece.Pawn, ['n'] = Piece.Knight, ['b'] = Piece.Bishop, ['r'] = Piece.Rook, ['q'] = Piece.Queen, ['k'] = Piece.King
             };
-            string boardSection = fen.Split(' ')[0];
+            string[] fenSections = fen.Split(' ');
             int file = 0;
             int rank = 7;
-            foreach(char c in boardSection) {
+            foreach(char c in fenSections[0]) {
                 if(c == '/') {
                     file = 0;
                     rank--;
@@ -43,8 +43,11 @@ namespace Chess {
                     }
                 }
             }
+            colorToMove = fenSections[1] == "w" ? 1 : 0;
         }
-        static MoveData() {
+        public static readonly int[] directionalOffsets = {8, -8, -1, 1, 7, -7, 9, -9};
+        public static int[,] squaresToEdge = new int[64,8];
+        static void MoveData() {
             for(int file = 0; file < 8; file++) {
                 for(int rank = 0; rank < 8; rank++) {
                     int north = 7 - rank;
@@ -52,18 +55,59 @@ namespace Chess {
                     int west = file;
                     int east = 7 - file;
                     int index = rank * 8 + file;
+                    squaresToEdge[index, 0] = north;
+                    squaresToEdge[index, 1] = south;
+                    squaresToEdge[index, 0] = west;
+                    squaresToEdge[index, 0] = east;
+                    squaresToEdge[index, 0] = Math.Min(north, west);
+                    squaresToEdge[index, 0] = Math.Min(south, east);
+                    squaresToEdge[index, 0] = Math.Min(north, east);
+                    squaresToEdge[index, 0] = Math.Min(south, west);
                 }
             }
         }
         public List<Move> GetLegalMoves() {
             List<Move> moves = new();
             for(int startSquare = 0; startSquare < 64; startSquare++) {
-
+                int currentPiece = squares[startSquare];
+                // checks if it's the right color
+                if((currentPiece & 0b1000) == colorToMove) {
+                    // a check if it's a sliding piece
+                    if((currentPiece & 0b0111) > 2 && (currentPiece & 0b0111) != 6) {
+                        int startDirection = (currentPiece & 0b0111) == 3 ? 4 : 0;
+                        int endDirection = (currentPiece & 0b0111) == 4 ? 8 : 4;
+                        for(int direction = startDirection; direction < endDirection; direction++) {
+                            for(int i = 0; i < squaresToEdge[startSquare, direction]; i++) {
+                                int targetSquareIndex = startSquare + directionalOffsets[direction] * (i + 1);
+                                int targetPiece = squares[targetSquareIndex];
+                                // can't capture the piece at the end of the line, end the cycle
+                                if((targetPiece & 0b1000) == colorToMove) {
+                                    break;
+                                }
+                                moves.Add(new Move(startSquare, targetSquareIndex));
+                                // the piece at the end of the line can be captured, you just added the capture to the list, end the cycle
+                                if((targetPiece & 0b1000) != colorToMove) {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
             }
+            return moves;
         }
     }
     public struct Move {
         public int StartSquare;
         public int EndSquare;
+        /// <summary>
+        /// Makes a new move with the starting and ending squares.
+        /// </summary>
+        /// <param name="start">Starting square of the move</param>
+        /// <param name="end">Ending square of the move</param>
+        public Move(int start, int end) {
+            StartSquare = start;
+            EndSquare = end;
+        }
     }
 }
---
+++ ChessEngine/Tests/Test1.cs
@@ -0,0 +1,7 @@
+using Chess;
+
+public class Test1 {
+    public static void Main() {
+        Board board = new("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    }
+}
