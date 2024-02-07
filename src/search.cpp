febf3f2cf1b08bf9f65c8a7bcf6a1017d218d3d5
Vast342 <jcpasfield@gmail.com>
2024-02-07 16:46:36 -0500

    Revert to -2

--- src/search.cpp
+++ src/search.cpp
@@ -514,7 +514,7 @@ int Engine::negamax(Board &board, int depth, int alpha, int beta, int ply, bool 
             } else if(sBeta >= beta) {
                 return sBeta;
             } else if (entry->score >= beta) {
-                TTExtensions = -1;
+                TTExtensions = -2;
             }
         }
         if(!board.makeMove(move)) {
