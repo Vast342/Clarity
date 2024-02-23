50e14044cf97a15960659aa2d64063484ba9277a
Vast342 <78440113+Vast342@users.noreply.github.com>
2024-02-23 11:45:57 -0500

    Update board.cpp

--- src/board.cpp
+++ src/board.cpp
@@ -517,21 +517,19 @@ int Board::getMovesQSearch(std::array<Move, 256> &moves) {
         }
     }
 	// pawn pushes
-    uint64_t pawnBitboard = getColoredPieceBitboard(colorToMove, Pawn);
-    uint64_t emptyBitboard = ~occupiedBitboard;
-    uint64_t pawnPushes = getPawnPushes(pawnBitboard, emptyBitboard, colorToMove);
+    const uint64_t pawnBitboard = getColoredPieceBitboard(colorToMove, Pawn);
+    const uint64_t emptyBitboard = ~occupiedBitboard;
+    const uint64_t pawnPushes = getPawnPushes(pawnBitboard, emptyBitboard, colorToMove);
     uint64_t pawnPushPromotions = pawnPushes & getRankMask(7 * colorToMove);
-    pawnPushes ^= pawnPushPromotions;
     while(pawnPushPromotions != 0) {
-        uint8_t index = popLSB(pawnPushPromotions);
-        uint8_t startSquare = (index + directionalOffsets[colorToMove]);
+        const uint8_t index = popLSB(pawnPushPromotions);
+        const uint8_t startSquare = (index + directionalOffsets[colorToMove]);
         for(int type = Knight; type < King; type++) {
             moves[totalMoves] = Move(startSquare, index, promotions[type-1]);
             totalMoves++;
         }
     }
     // pawn captures
-    const uint64_t pawnBitboard = getColoredPieceBitboard(colorToMove, Pawn);
     uint64_t capturable = state.coloredBitboards[1 - colorToMove];
     if(state.enPassantIndex != 64) {
         capturable |= squareToBitboard[state.enPassantIndex];
