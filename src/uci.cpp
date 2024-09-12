83ca70792cd046d23a31b48831063925c29953a8
Joseph Pasfield <78440113+Vast342@users.noreply.github.com>
2024-09-12 13:48:30 -0400

    thing 2

--- src/uci.cpp
+++ src/uci.cpp
@@ -9,7 +9,7 @@
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See thfe
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
@@ -319,6 +319,7 @@ int main(int argc, char* argv[]) {
         std::getline(std::cin, command, '\n');
         if(mainThreadDone) {
             threads.clear();
+            mainThreadDone = false;
         }
         if(command == "quit") {
             if(threads.size() != 0) {
