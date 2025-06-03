46456ba807ba03858e7a34126c12dbcb4c5c38df
Joseph Pasfield <78440113+Vast342@users.noreply.github.com>
2025-06-03 10:07:38 -0400

    step 2

--- src/search.h
+++ src/search.h
@@ -73,7 +73,7 @@ struct Engine {
         TranspositionTable* TT;
 
         std::array<std::array<std::array<std::array<std::array<int16_t, 2>, 64>, 2>, 64>, 2> historyTable;
-        std::array<std::array<std::array<std::array<int16_t, 7>, 64>, 7>, 2> noisyHistoryTable;
+        std::array<std::array<std::array<std::array<std::array<int16_t, 2>, 7>, 64>, 7>, 2> noisyHistoryTable;
         std::array<std::array<std::array<std::array<int16_t, 7>, 64>, 7>, 2> qsHistoryTable;
         std::array<std::array<std::array<std::array<int16_t, 64>, 7>, 2>, 32768> pawnHistoryTable;
         Corrhist corrhist;
@@ -90,7 +90,7 @@ struct Engine {
         void scoreMovesQS(const Board& board, std::array<Move, 256> &moves, std::array<int, 256> &values, int numMoves, Move ttMove);
         int16_t qSearch(Board &board, int alpha, int beta, int16_t ply);
         void updateHistory(const int colorToMove, const int start, const int end, const int piece, const int bonus, const int16_t ply, const int hash, const bool startAttack, const bool endAttack);
-        void updateNoisyHistory(const int colorToMove, const int piece, const int end, const int victim, const int bonus);
+        void updateNoisyHistory(const int colorToMove, const int piece, const int end, const int victim, const int bonus, const bool endAttack);
         void updateQSHistory(const int colorToMove, const int piece, const int end, const int victim, const int bonus);
         int16_t negamax(Board &board, int depth, int alpha, int beta, int16_t ply, bool nmpAllowed, bool isCutNode);
         std::string getPV();
