9b8e2c85c776fd6e0b243eddb48a7e655462d289
Joseph Pasfield <78440113+Vast342@users.noreply.github.com>
2025-07-05 21:01:30 -0400

    Killer Move (#175)
    ```
    Elo   | 29.50 +- 14.16 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 3.00 (-2.25, 2.89) [0.00, 10.00]
    Games | N: 850 W: 239 L: 167 D: 444
    Penta | [11, 72, 193, 132, 17]
    ```
    https://furybench.com/test/1520/
    
    bench: 4478119

--- src/history.h
+++ src/history.h
@@ -21,19 +21,20 @@ Clarity
 #include "tunables.h"
 
 struct HistoryTables {
-public:
-    inline void clear() {
+    static constexpr int quietHistCount = 1;
+    static constexpr int historyCap = 16384;
+    void clear() {
         std::memset(historyTable.data(), 0, sizeof(historyTable));
     }
     // handles 1: bonus for move, 2: penalties for other moves
-    inline void betaCutoff(const Board &board, const int colorToMove, const Move cutoffMove, const std::array<Move, 256> &testedMoves, const int legalMoves, const int depth) {
+    void betaCutoff(const Board &board, const int colorToMove, const Move cutoffMove, const std::array<Move, 256> &testedMoves, const int legalMoves, const int depth) {
         auto bonus = quietBonus(depth);
         // cutoff move bonus
         if(board.pieceAtIndex(cutoffMove.getEndSquare()) == None) {
-            auto start = cutoffMove.getStartSquare();
-            auto end = cutoffMove.getEndSquare();
-            auto startAttack = board.squareIsUnderAttack(start);
-            auto endAttack = board.squareIsUnderAttack(end);
+            const auto start = cutoffMove.getStartSquare();
+            const auto end = cutoffMove.getEndSquare();
+            const auto startAttack = board.squareIsUnderAttack(start);
+            const auto endAttack = board.squareIsUnderAttack(end);
             updateHistory(colorToMove, start, end, startAttack, endAttack, bonus);
         }
         bonus = -bonus;
@@ -41,20 +42,19 @@ public:
         for(int idx = 0; idx < legalMoves - 1; idx += 1) {
             const auto move = testedMoves[idx];
             if(board.pieceAtIndex(move.getEndSquare()) == None) {
-                auto start = cutoffMove.getStartSquare();
-                auto end = cutoffMove.getEndSquare();
-                auto startAttack = board.squareIsUnderAttack(start);
-                auto endAttack = board.squareIsUnderAttack(end);
+                const auto start = cutoffMove.getStartSquare();
+                const auto end = cutoffMove.getEndSquare();
+                const auto startAttack = board.squareIsUnderAttack(start);
+                const auto endAttack = board.squareIsUnderAttack(end);
                 updateHistory(colorToMove, start, end, startAttack, endAttack, bonus);
             }
         }
     }
-    inline void updateHistory(const int colorToMove, const int start, const int end, const bool startAttack, const bool endAttack, const bool bonus) {
+    void updateHistory(const int colorToMove, const int start, const int end, const bool startAttack, const bool endAttack, const bool bonus) {
         update(&historyTable[colorToMove][start][startAttack][end][endAttack], bonus);
     }
     std::array<std::array<std::array<std::array<std::array<int16_t, 2>, 64>, 2>, 64>, 2> historyTable;
 private:
-    static constexpr int historyCap = 16384;
     static void update(int16_t *value, const int bonus) {
         const auto thingToAdd = bonus - *value * std::abs(bonus) / historyCap;
         *value += thingToAdd;
